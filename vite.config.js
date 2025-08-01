c75f3f1ecc81c1fb00ccc100ccfbf78e2067c76e
stato10 <stato10@github.com>
2025-08-01 14:21:52 +0300

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PerfectDress/',
   server: {
-    port: 3000,
-    open: true
+    port: 3000
   }
 }) 
